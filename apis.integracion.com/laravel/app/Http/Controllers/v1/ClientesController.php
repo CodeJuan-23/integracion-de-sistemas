2e8294a2d36d0cd9cccd71e4adcc745c0df5edd4
Ju4n-22 <i2226338@continental.edu.pe>
2022-08-17 06:15:41 -0500

    creacion del controlador y modelo

---
+++ apis.integracion.com/laravel/app/Http/Controllers/v1/ClientesController.php
@@ -0,0 +1,16 @@
+<?php
+ 
+namespace App\Http\Controllers\v1;
+ 
+use App\Http\Controllers\Controller;
+
+//use App\Models\User;
+ 
+class ClientesController extends Controller
+{
+    
+    public function getAll()
+    {
+        return "Hola mundo";
+    }
+}
